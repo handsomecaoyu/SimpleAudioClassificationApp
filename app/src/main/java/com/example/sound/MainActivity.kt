e5662bbcd41a7544499d67233cc7f2767f2244fa
caoyu <1755475455@qq.com>
2022-05-05 22:59:54 +0800

    实现录音功能

--- app/src/main/java/com/example/sound/MainActivity.kt
+++ app/src/main/java/com/example/sound/MainActivity.kt
@@ -1,11 +1,135 @@
 package com.example.sound
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaPlayer
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.example.sound.databinding.ActivityMainBinding
+import java.io.IOException
+
+
 
 class MainActivity : AppCompatActivity() {
+    private val LOG_TAG = "AudioRecordTest"
+    private val REQUEST_RECORD_AUDIO_PERMISSION = 200
+    private lateinit var binding: ActivityMainBinding
+    private var fileName: String = ""
+    private var recorder: MediaRecorder? = null
+    private var player: MediaPlayer? = null
+
+    // Requesting permission to RECORD_AUDIO
+    private var permissionToRecordAccepted = false
+    private var permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionToRecordAccepted = if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        } else {
+            false
+        }
+        if (!permissionToRecordAccepted) finish()
+    }
+
+    private fun onRecord(start: Boolean) = if (start) {
+        startRecording()
+    } else {
+        stopRecording()
+    }
+
+    private fun onPlay(start: Boolean) = if (start) {
+        startPlaying()
+    } else {
+        stopPlaying()
+    }
+
+    private fun startPlaying() {
+        player = MediaPlayer().apply {
+            try {
+                setDataSource(fileName)
+                prepare()
+                start()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+        }
+    }
+
+    private fun stopPlaying() {
+        player?.release()
+        player = null
+    }
+
+    private fun startRecording() {
+        recorder = MediaRecorder().apply {
+            setAudioSource(MediaRecorder.AudioSource.MIC)
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
+            setOutputFile(fileName)
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)
+
+            try {
+                prepare()
+            } catch (e: IOException) {
+                Log.e(LOG_TAG, "prepare() failed")
+            }
+
+            start()
+        }
+    }
+
+    private fun stopRecording() {
+        recorder?.apply {
+            stop()
+            release()
+        }
+        recorder = null
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val recordView = binding.root
+        setContentView(recordView)
+
+        // Record to the external cache directory for visibility
+        fileName = "${externalCacheDir?.absolutePath}/audiometers.3gp"
+
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION)
+        Log.i(LOG_TAG, "start")
+        var startRecordingFlag = true
+        binding.recordBtn.setOnClickListener{
+            Log.i(LOG_TAG, "record")
+
+            onRecord(startRecordingFlag)
+            binding.recordBtn.text = when (startRecordingFlag) {
+                true -> "Stop recording"
+                false -> "Start recording"
+            }
+            startRecordingFlag = !startRecordingFlag
+        }
+
+        var startPlayingFlag = true
+        binding.playBtn.setOnClickListener {
+            onPlay(startPlayingFlag)
+            binding.playBtn.text = when (startPlayingFlag) {
+                true -> "Stop playing"
+                false -> "Start playing"
+            }
+            startPlayingFlag = !startPlayingFlag
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        recorder?.release()
+        recorder = null
+        player?.release()
+        player = null
     }
 }
